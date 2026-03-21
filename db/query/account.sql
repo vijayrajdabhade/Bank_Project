c5518c696feb068fab0ad92e2b9cd4a20be54c98
vijayrajdabhade <vijayrajdabhade@gmail.com>
2026-03-21 13:25:28 +0000

    Generate curd golang from sqlc

---
+++ db/query/account.sql
@@ -0,0 +1,25 @@
+-- name: CreateAccount :one
+INSERT INTO account (
+  owner, balance, currency
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1;
+
+-- name: ListAccounts :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE account 
+SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+ WHERE id = $1;
